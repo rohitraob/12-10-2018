b312932b1430db9c65f195c4e2bf4ef82b0250d0
rohitroab <rohit.boyinapalli@gmail.com>
2018-12-10 17:11:56 +0530

    intintal commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  Hello = "Can You Please Enter Your Email";
+  Hello1 = "Can You Please Enter Your Email";
+
+  firstName : string = "Pradeep";
+  lastName : string = "Rao";  
+  badHtml : string = "Hello <script> alert('Hello')</script> World";
+
+ getfullName(): string {
+    return this.firstName + ' ' + this.lastName
+  } 
+
+
+ details = ""; 
+
+
+
+  userData = {
+    name:"Ram",
+    pass:""
+}
+
+  getDetails(){
+      console.log(this.userData);
+      alert('Hello')
+  } 
+
+   employeeDetails : any = [];
+
+    exEmp : any = [];
+
+  constructor(){
+
+    this.employeeDetails = [   
+          {empCode:"10732",name:"Rohit",salary:"405500",gender:"Male",dob:"10-10-2018",},
+          {empCode:"10733",name:"Rohit Rao",salary:"400110",gender:"Male",dob:"10-10-2018",},
+          {empCode:"10734",name:"RAM",salary:"40200",gender:"Male",dob:"10-10-2018",},
+          {empCode:"10735",name:"Hello",salary:"4000",gender:"Male",dob:"10-10-2018",},
+          {empCode:"10736",name:"Rmesh",salary:"40600",gender:"Male",dob:"10-10-2018",}
+
+  ],
+
+    this.exEmp = [
+          {empCode:"10732",name:"Ra",salary:"405500",gender:"Male",dob:"10-10-2018",},
+          {empCode:"10733",name:"Rohit Rao",salary:"400110",gender:"Male",dob:"10-10-2018",},
+          {empCode:"10734",name:"RAM",salary:"40200",gender:"Male",dob:"10-10-2018",},
+          {empCode:"10735",name:"Hello",salary:"4000",gender:"Male",dob:"10-10-2018",},
+          {empCode:"10736",name:"Rmesh",salary:"40600",gender:"Male",dob:"10-10-2018",}
+    ]
+
+  }
+
+
+    
+
+  showPass(){
+     this.details = this.userData['pass'];
+  }
+
+
+
+
 }
